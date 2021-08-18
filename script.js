6bb261966fa913730bfe05d5f0a7c4c85a890a46
Ecris ton nom ici, en gardant les quotes <Ecris ton email ici, en gardant les quotes>
2021-08-19 00:25:14 +0200

    Startup nation 1 OK

--- script.js
+++ script.js
@@ -52,14 +52,62 @@ function pyramideMario() {
 
 }
 
-
-
 pyramide = document.getElementById('pyramide');
 pyramide.addEventListener('click', pyramideMario);
 
 // Fonction Startup
 function startupNation() {
-  console.log("Bonjour Monde");
+
+  const entrepreneurs = [
+    { first: 'Steve', last: 'Jobs', year: 1955 },
+    { first: 'Oprah', last: 'Winfrey', year: 1954 },
+    { first: 'Bill', last: 'Gates', year: 1955 },
+    { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+    { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+    { first: 'Beyonce', last: 'Knowles', year: 1981 },
+    { first: 'Jeff', last: 'Bezos', year: 1964 },
+    { first: 'Diane', last: 'Hendricks', year: 1947 },
+    { first: 'Elon', last: 'Musk', year: 1971 },
+    { first: 'Marissa', last: 'Mayer', year: 1975 },
+    { first: 'Walt', last: 'Disney', year: 1901 },
+    { first: 'Larry', last: 'Page', year: 1973 },
+    { first: 'Jack', last: 'Dorsey', year: 1976 },
+    { first: 'Evan', last: 'Spiegel', year: 1990 },
+    { first: 'Brian', last: 'Chesky', year: 1981 },
+    { first: 'Travis', last: 'Kalanick', year: 1976 },
+    { first: 'Marc', last: 'Andreessen', year: 1971 },
+    { first: 'Peter', last: 'Thiel', year: 1967 }
+  ];
+
+  let entrepreneurs_count = entrepreneurs.length;
+  let seventies = [];
+  let names = [];
+  const entrepreneur = { first: "", last: "" }
+
+  console.log("Quels sont les entrepreneurs nés dans les années 70 ?");
+
+  for (let count = 0; count <= entrepreneurs_count - 1; count++) {
+    if (entrepreneurs[count].year <= 1979 && entrepreneurs[count].year >= 1970) {
+      seventies.push(entrepreneurs[count].last);
+    }
+  }
+  console.log(seventies);
+
+  console.log("Voici le prénom et le nom des entrepreneurs");
+
+  for (let count = 0; count <= entrepreneurs_count - 1; count++) {
+    names[count] = entrepreneur
+    names[count].first = entrepreneurs[count].first;
+    names[count].last = entrepreneurs[count].last;
+  }
+
+  console.log(names)
+  console.log(entrepreneurs)
+
+
+  console.log("Quel âge aurait chaque inventeur aujourd'hui ?");
+  console.log("Voici les entrepreneurs par ordre alphabétique du nom de famille");
+
 }
 
 startup = document.getElementById('startup');
