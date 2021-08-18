da8f3b7db0b9514a346c6d108bbe307e94f49ef3
Ecris ton nom ici, en gardant les quotes <Ecris ton email ici, en gardant les quotes>
2021-08-18 18:01:50 +0200

    branchements JS OK

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 console.log("coucou depuis la console");
+
+// Fonction bonjour Monde
+function helloWorld() {
+  console.log("Bonjour Monde");
+}
+
+bonjour = document.getElementById('bonjour_monde');
+bonjour.addEventListener('click', helloWorld);
+
+// Fonction salut
+function salut() {
+  console.log("Bonjour Monde");
+}
+
+salut_var = document.getElementById('salut');
+salut_var.addEventListener('click', salut);
+
+// Fonction Calcul
+function calculRapide() {
+  console.log("Bonjour Monde");
+}
+
+calcul = document.getElementById('calcul');
+calcul.addEventListener('click', calculRapide);
+
+// Fonction Pyramide
+function pyramideMario() {
+  console.log("Bonjour Monde");
+}
+
+pyramide = document.getElementById('pyramide');
+pyramide.addEventListener('click', pyramideMario);
+
+// Fonction Startup
+function startupNation() {
+  console.log("Bonjour Monde");
+}
+
+startup = document.getElementById('startup');
+startup.addEventListener('click', startupNation);
+
+// Fonction Biblio
+function bibliothecaire() {
+  console.log("Bonjour Monde");
+}
+
+biblio = document.getElementById('biblio');
+biblio.addEventListener('click', bibliothecaire);
+
+// Fonction Code
+function lifeCode() {
+  console.log("Bonjour Monde");
+}
+
+life = document.getElementById('life_code');
+life.addEventListener('click', lifeCode);
+
+// Fonction Bot
+function acneBot() {
+  console.log("Bonjour Monde");
+}
+
+bot = document.getElementById('bot');
+bot.addEventListener('click', acneBot);
